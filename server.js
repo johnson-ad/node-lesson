8726361b448970af0d7bdd811414b1fdd5258602
johnson-ad <one007stone@gmail.com>
2023-08-24 19:06:27 +0000

    Ajout du middleware express.json() pour afficher le body de la requiete

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-
+//on utilise les middleware express.json() pour afficher le body de la requiete
+app.use(express.json())
 
 //on utilise les middleware avec la methode use()
 app.use("/api/contacts", require("./routes/contactRoutes"))
