fd6d4f0d666311b314e077bffb4bc2e5ed63d8c9
johnson-ad <one007stone@gmail.com>
2023-08-24 18:25:15 +0000

    Creations des route -get all contact -get one contact -create contact -update contact -delete contact

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
+//on utilise les middleware avec la methode use()
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
