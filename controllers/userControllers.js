8e242dd4a7336e3c742e0432172424614731d620
johnson-ad <one007stone@gmail.com>
2023-08-25 16:11:20 +0000

    Access token with jwt

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
 //@desc Register a user
 //@route POST /api/users/register
 //@acces public
 const registerUser = asyncHandler(async ( req, res)=>{
-    
+    const { username, email, password} = req.body
     if(!username || !email || !password){
         res.status(400)
         throw new Error("All fields are mandatory !")
@@ -46,7 +47,33 @@ const registerUser = asyncHandler(async ( req, res)=>{
 //@route POST /api/users/login
 //@acces public
 const loginUser = asyncHandler(async ( req, res)=>{
-    res.json({message : "Login user"})
+    const { email, password} = req.body
+    if(!email || !password){
+        res.status(400)
+        throw new Error("All fields are mandatory !")
+    }
+         
+    const user = await User.findOne({email})
+
+    //Compare password witch hashedpassword
+    if(user && ( await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            // My payload is user
+            user : {
+                username : user.username,
+                email : user.email,
+                id : user.id
+            }
+        }, process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn : "1m"
+        })
+
+        res.status(200).json({ accessToken })
+    }else {
+        res.status(401)
+        throw new Error("email or password is not valid")
+    }
+      
 })
 
 
